189d06c93b70fde2ee77dc74d0c36ea919f2eb59
Rishu Kumar <126085066+rishu-7549@users.noreply.github.com>
2025-02-07 11:35:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+window.onload=function(){
+	let ele = document.getElementById("level");
+	let index = 0;
+	 while (ele) {
+        index++;
+        ele = ele.parentElement;
+    }
+    alert("The level of the element is: " + index);
+}
+
